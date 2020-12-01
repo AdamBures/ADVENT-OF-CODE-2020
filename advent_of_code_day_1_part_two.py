a853fc96071930e17f84bc0593472cb1dbcf20a3
AdamBures <47189693+AdamBures@users.noreply.github.com>
2020-12-01 22:52:47 +0100

    Update advent_of_code_day_1_part_two.py

--- advent_of_code_day_1_part_two.py
+++ advent_of_code_day_1_part_two.py
@@ -1,23 +1,28 @@
 import os
 import itertools
 
-os.chdir("Text input")
-lst = []
+class Main(object):
+    os.chdir("Text input")
 
-def report_repair(lst):
-    year = 2020
-    length = 3
-    for numbers in itertools.permutations(lst, length):
-        if sum(numbers) == 2020:
-            result = numbers[0]*numbers[1]*numbers[2]
-            print(result)
-            break
-        
-def read_file():
-    file = open("report_repair.txt")
-    for number in file:
-        lst.append(int(number))
-    report_repair(lst)
+    def __init__(self):
+        self.year = 2020
+        self.length = 3
+        self.lst = []
+        self.path = "report_repair.txt"
+        self.result = 0
+
+    def report_repair(self, lst):
+        for self.numbers in itertools.permutations(lst,self.length):
+            if sum(self.numbers) == self.year:
+                self.result = self.numbers[0] * self.numbers[1] * self.numbers[2]
+                print(self.result)
+                break
+    
+    def read_file(self):
+        with open(self.path, "r") as file:
+            for number in file:
+                self.lst.append(int(number))
+            Main().report_repair(self.lst)
 
 if __name__ == "__main__":
-    read_file()
+    Main().read_file()
